0158ff8e964a5dc828eb1b1c47334861c2f6c6ea
1mperium <vanyalisenko1985@gmail.com>
2024-12-30 22:13:16 +0300

    Создал серверную часть

---
+++ server/src/main/java/com/im/reservio/ReservioApplication.java
@@ -0,0 +1,13 @@
+package com.im.reservio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservioApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/im/reservio/ReservioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.im.reservio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
